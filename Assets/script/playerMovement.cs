fddcec26df49dccfc13fd720b356c4b80445da00
LambFerret <dignrjrl12@gmail.com>
2021-07-23 16:36:12 +0900

    20210723 afternoon

--- Assets/script/playerMovement.cs
+++ Assets/script/playerMovement.cs
@@ -7,20 +7,30 @@ public class playerMovement : MonoBehaviour
     public GameObject cam;
     Rigidbody rb;
     public float speed;
-    Animation ani;
+    public float jump;
+    Animator ani;
+    bool isAir = true;
     // Start is called before the first frame update
     void Start()
     {
 
-        ani = gameObject.GetComponent<Animation>();
+        ani = gameObject.GetComponent<Animator>();
         rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerLootAt();
-        Move();
+        if (Input.GetMouseButton(1))
+        {
+            PlayerLootAt();
+        }
+        if (!isAir)
+        {
+            Move();
+            Jump();
+
+        }
     }
 
 
@@ -34,9 +44,48 @@ public class playerMovement : MonoBehaviour
 
     void Move()
     {
+        ani.SetBool("isMoving", true);
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         Vector3 normalVec = (transform.right * h + transform.forward * v).normalized;
-        rb.velocity = (normalVec)*speed;
+        rb.velocity = (normalVec) * speed;
+    }
+
+    void Jump()
+    {
+
+        if (Input.GetKeyDown("space"))
+        {
+            ani.SetBool("isJumping", true);
+            Vector3 jumpVel = rb.velocity;
+            jumpVel.y = jump;
+            Debug.Log(jumpVel);
+            rb.AddForce(jumpVel, ForceMode.Impulse);
+
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            isAir = false;
+
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            isAir = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 6)
+        {
+            isAir = true;
+        }
     }
 }
