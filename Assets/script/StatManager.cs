52acdfcd58bf08a827d0d5399155d36af94ab3c2
CC715075 <y2w9267@gmail.com>
2021-07-25 22:54:31 +0900

    20210725

--- Assets/script/StatManager.cs
+++ Assets/script/StatManager.cs
@@ -1,23 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
 
-public class playerBasic : MonoBehaviour
+public class StatManager : MonoBehaviour
 {
+    readonly string xmlFileName = "StatTable";
 
+    private void LoadXML(string _fileName)
+    {
+        TextAsset txtAsset = (TextAsset)Resources.Load("C:/Users/User/New Unity Project/Assets/xml/" + _fileName );
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.LoadXml(txtAsset.text);
+        XmlNodeList costTable = xmlDoc.GetElementsByTagName("text");
+        foreach (XmlNode i in costTable)
+        {
+            Debug.Log(i);
+
+        }
+
+    }
     // singleton
-    private static playerBasic _instance;
-    public static playerBasic Instance { get { return _instance; } }
+    private static StatManager _instance;
+    public static StatManager Instance { get { return _instance; } }
     private void Awake()
     {
+        
+
         if (_instance != null && _instance != this)
         {
-            Destroy(this.gameObject);
+            Destroy(gameObject);
         } else
         {
             _instance = this;
         }
     }
+    
 
     
     public int health = 50;
@@ -27,7 +45,6 @@ public class playerBasic : MonoBehaviour
     public float attackSpeed = 0.5f;
 
     private float currentValue;
-
     public float myMaxValue { get; set; }
 
     public float myCurrentValue
@@ -48,7 +65,7 @@ public class playerBasic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        LoadXML(xmlFileName);
     }
 
     // Update is called once per frame
@@ -56,7 +73,5 @@ public class playerBasic : MonoBehaviour
     {
         
     }
-    public void StatManager(int health, int mana)
-    {
-    }
+    
 }
