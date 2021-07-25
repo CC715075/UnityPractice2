f999ee23b361dafedc7a7bda1e6713ca15cd7a57
LambFerret <dignrjrl12@gmail.com>
2021-07-25 13:28:08 +0900

    20210725 v1

--- Assets/script/playerBasic.cs
+++ Assets/script/playerBasic.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class playerBasic : MonoBehaviour
 {
+
+    // singleton
+    private static playerBasic _instance;
+    public static playerBasic Instance { get { return _instance; } }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            _instance = this;
+        }
+    }
+
+    
     public int health = 50;
     public int mana = 50;
 
     public int attack = 10;
     public float attackSpeed = 0.5f;
+
+    private float currentValue;
 
     public float myMaxValue { get; set; }
 
@@ -18,12 +36,19 @@ public class playerBasic : MonoBehaviour
         {
             return currentValue;
         }
+
+        set
+        {
+            if (value > myMaxValue) currentValue = myMaxValue;
+            else if (value < 0) currentValue = 0;
+            else currentValue = value;
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
