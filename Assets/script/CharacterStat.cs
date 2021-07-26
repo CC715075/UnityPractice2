3a499ad80b0f91f09a92211f02c7e3828e9e6a32
CC715075 <y2w9267@gmail.com>
2021-07-26 21:05:52 +0900

    20210726 final

---
+++ Assets/script/CharacterStat.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour
+{
+    public Stat damage;
+    public Stat armor;
+
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        currentHealth -= damage;
+        Debug.Log(transform.name + "take" + damage + "damage.");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + "die");
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+}
--- Assets/script/StatManager.cs
+++ Assets/script/StatManager.cs
@@ -6,72 +6,72 @@ using System.Xml;
 public class StatManager : MonoBehaviour
 {
     readonly string xmlFileName = "StatTable";
+    private int playerAttack;
+    private float playerAttackSpeed;
+    private int playerHP;
+    private int playerMP;
+    private int playerindex = 0;
 
-    private void LoadXML(string _fileName)
-    {
-        TextAsset txtAsset = (TextAsset)Resources.Load("C:/Users/User/New Unity Project/Assets/xml/" + _fileName );
-        XmlDocument xmlDoc = new XmlDocument();
-        xmlDoc.LoadXml(txtAsset.text);
-        XmlNodeList costTable = xmlDoc.GetElementsByTagName("text");
-        foreach (XmlNode i in costTable)
-        {
-            Debug.Log(i);
 
-        }
-
-    }
     // singleton
     private static StatManager _instance;
     public static StatManager Instance { get { return _instance; } }
     private void Awake()
     {
-        
-
         if (_instance != null && _instance != this)
         {
             Destroy(gameObject);
-        } else
+        }
+        else
         {
             _instance = this;
         }
-    }
-    
-
-    
-    public int health = 50;
-    public int mana = 50;
-
-    public int attack = 10;
-    public float attackSpeed = 0.5f;
-
-    private float currentValue;
-    public float myMaxValue { get; set; }
-
-    public float myCurrentValue
-    {
-        get
-        {
-            return currentValue;
-        }
-
-        set
-        {
-            if (value > myMaxValue) currentValue = myMaxValue;
-            else if (value < 0) currentValue = 0;
-            else currentValue = value;
-        }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
         LoadXML(xmlFileName);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void LoadXML(string _fileName)
     {
-        
+        TextAsset txtAsset = Resources.Load<TextAsset>("xml/" + _fileName);
+        XmlDocument xmlDoc = new XmlDocument();
+
+        xmlDoc.LoadXml(txtAsset.text);
+        XmlNodeList costTable = xmlDoc.GetElementsByTagName("text");
+
+        XmlNode i = costTable[playerindex];
+        playerAttack = int.Parse(i.SelectSingleNode("attack").InnerText);
+        playerAttackSpeed = float.Parse(i.SelectSingleNode("attackSpeed").InnerText);
+        playerHP = int.Parse(i.SelectSingleNode("hp").InnerText);
+        playerMP = int.Parse(i.SelectSingleNode("mp").InnerText);
+    }
+
+
+    // making Columns name of XML
+    private List<string> ListColumns(XmlNodeList costTable)
+    {
+        List<string> list = new List<string>();
+        XmlNode i = costTable[playerindex];
+        string str = i.FirstChild.Name;
+        list.Add(str);
+        while (true)
+        {
+            try
+            {
+                list.Add(i.SelectSingleNode(str).NextSibling.Name);
+                str = i.SelectSingleNode(str).NextSibling.Name;
+            }
+            catch { break; }
+        }
+        return list;
+    }
+    public void LabelingStat(List<string> list)
+    {
+        foreach(string i in list)
+        {
+            name = "player" + i;
+
+        }
     }
     
+
 }
